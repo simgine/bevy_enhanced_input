f65d0a027e3e0000b994b19ae6d67f4b532bde69
Charles Bournhonesque <charlesbour@gmail.com>
2025-08-28 21:19:16 -0400

    add doc comment

--- src/action.rs
+++ src/action.rs
@@ -259,6 +259,8 @@ pub struct ActionTime {
 }
 
 impl ActionTime {
+
+    /// Updates the timers based on the given delta time and action state.
     pub fn update(&mut self, delta_secs: f32, state: ActionState) {
         match state {
             ActionState::None => {
