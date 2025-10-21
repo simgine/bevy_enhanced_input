860a5dfd9384b8142a3eb05ee782cb58b1c74c0c
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-10-21 19:39:17 +0300

    Minor adjustments for "putting it all togeter" example (#233)
    - We use `App`, but create a new `World` below, which is confusing.
    - Apply formatting. I wish `cargo fmt` work with examples.

--- src/lib.rs
+++ src/lib.rs
@@ -103,11 +103,11 @@ struct Jump;
 struct Fire;
 
 let mut app = App::new();
-app.add_plugins(EnhancedInputPlugin);
-app.add_input_context::<Player>();
+app.add_plugins(EnhancedInputPlugin)
+    .add_input_context::<Player>()
+    .finish();
 
-let mut world = World::new();
-world.spawn((
+app.world_mut().spawn((
     Player,
     actions!(Player[
         (
@@ -120,7 +120,6 @@ world.spawn((
         ),
     ])
 ));
-
 ```
 
 If we wanted to add a new context for when the player is in a vehicle, we would create a new context component `InVehicle`
